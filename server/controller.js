861d27359ec9047efb7c22ef2d1624b598d8de3a
Sam Forbyn <samforbyn@gmail.com>
2021-11-30 13:37:00 -0700

    init commit

---
+++ server/controller.js
@@ -0,0 +1,39 @@
+const houses = require('./db.json')
+let globalId = 4
+
+module.exports = {
+    getHouses: (req, res) => res.status(200).send(houses),
+    deleteHouse: (req, res) => {
+        let index = houses.findIndex(elem => elem.id === +req.params.id)
+        houses.splice(index, 1)
+        res.status(200).send(houses)
+
+    },
+    createHouse: (req, res) => {
+        let {address, price, imageURL} = req.body
+        let newHouse = {
+            address,
+            price,
+            imageURL,
+            id: globalId
+        }
+        houses.push(newHouse)
+        res.status(200).send(houses)
+        globalId++
+    },
+    updateHouse: (req, res) => {
+        let {id} = req.params
+        let {type} = req.body
+        let index = houses.findIndex(elem => +elem.id === +id)
+
+        if(type === "minus"){
+            houses[index].price -= 10000
+            res.status(200).send(houses)
+        }else if(type === "plus"){
+            houses[index].price += 10000
+            res.status(200).send(houses)
+        }else{
+            res.sendStatus(400)
+        }
+    }
+}
